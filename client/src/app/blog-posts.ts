367e3c532c7c00e3373fe686375b15306fe61e29
marceltheshell <marcel.degas@gmail.com>
2017-12-06 19:51:21 -0800

    adds a mock list of blog posts

--- client/src/app/blog-posts.ts
+++ client/src/app/blog-posts.ts
@@ -1,18 +1,18 @@
-import { BlogPost } from './models/blogPost'
+import { BlogPost } from './models/blogPost';
 
-const mockBlogPosts: BlogPost[] = [
+export const mockBlogPosts: BlogPost[] = [
 	{
-		id: 1;
-		title:"My First";
-		author: "Marcel";
-		date: "2/20/81";
-		views: "";
+		id: 1,
+		title:"My First",
+		author: "Marcel",
+		date: "2/20/81",
+		views: ""
 	},
 	{
-		id: 2;
-		title:"My Second";
-		author: "Humberto";
-		date: "9/99/81";
-		views: "";
+		id: 2,
+		title:"My Second",
+		author: "Humberto",
+		date: "9/99/81",
+		views: ""
 	}
-]
+];
