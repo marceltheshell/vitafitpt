8964f0033083e732c7881c38a0e0f6fb53d0a415
marceltheshell <marcel.degas@gmail.com>
2018-01-17 22:43:46 -0800

    moves contact into its own component

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientService } from '../http-client-service/http-client.service';
+import { Contact } from '../models/contact';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClientService: HttpClientService) { }
 
   ngOnInit() {
+    this.getContacts();
+  }
+
+  getContacts(): void {
+    this.httpClientService.getAllContacts().subscribe(
+      data => { this.contacts = data }, 
+      err => console.error(err)
+    )
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientService } from '../http-client-service/http-client.service';
-import { Contact } from '../models/contact';
 import * as $ from 'jquery';
 
 
@@ -16,7 +14,7 @@ export class HomeComponent implements OnInit {
 
   public contacts;
 
-  constructor(private httpClientService: HttpClientService) {}
+  constructor() {}
 
   ngOnInit() {
 
@@ -31,15 +29,6 @@ export class HomeComponent implements OnInit {
         });
       });
     });
-
-    this.getContacts();
-
-  }
-
-  getContacts(): void {
-    this.httpClientService.getAllContacts().subscribe(
-      data => { this.contacts = data }, 
-      err => console.error(err)
-    )
+    
   }
 }
